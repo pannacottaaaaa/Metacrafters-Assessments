7dcb86631270e48cf155d00220b39ada6017c562
pannacottaaaaa <mrjccaranay@mymail.mapua.edu.ph>
2024-07-03 11:12:31 +0800

    Update Assessment.sol

--- ETH + AVAX Intermediate/Smart Contract Management/Assessment.sol
+++ ETH + AVAX Intermediate/Smart Contract Management/Assessment.sol
@@ -1,60 +1,57 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
+contract SimpleGame {
+    address public owner;
+    uint256 public totalTokens;
+    uint256 public gameCost;
+    uint256 public winningNumber;
+    
+    event GamePlayed(address indexed player, uint256 guess, bool win, uint256 reward);
+    event TokensClaimed(address indexed player, uint256 amount);
 
-contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+    mapping(address => uint256) public playerTokens;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    constructor(uint256 _initialTokens, uint256 _gameCost) {
+        owner = msg.sender;
+        totalTokens = _initialTokens;
+        gameCost = _gameCost;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
-    }
-
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
-    }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+    function playGame(uint256 guess) public payable {
+        require(msg.value == gameCost, "Incorrect game cost");
+        require(guess >= 1 && guess <= 10, "Guess must be between 1 and 10");
+        
+        // Generate a winning number between 1 and 10
+        winningNumber = (uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 10) + 1;
+        
+        bool win = (guess == winningNumber);
+        uint256 reward = 0;
+        
+        if (win) {
+            reward = gameCost * 2;
+            require(totalTokens >= reward, "Not enough tokens in the contract");
+            playerTokens[msg.sender] += reward;
+            totalTokens -= reward;
         }
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        emit GamePlayed(msg.sender, guess, win, reward);
     }
+
+    function claimTokens() public {
+        uint256 tokens = playerTokens[msg.sender];
+        require(tokens > 0, "No tokens to claim");
+        
+        playerTokens[msg.sender] = 0;
+        payable(msg.sender).transfer(tokens);
+        
+        emit TokensClaimed(msg.sender, tokens);
+    }
+
+    function addTokens(uint256 amount) public {
+        require(msg.sender == owner, "Only the owner can add tokens");
+        totalTokens += amount;
+    }
+
+    receive() external payable {}
 }
