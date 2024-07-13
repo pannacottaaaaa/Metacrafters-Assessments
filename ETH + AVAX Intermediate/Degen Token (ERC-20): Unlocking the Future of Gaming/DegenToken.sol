be0a2278d3b57d476fb3ad077b74e261dafe3daa
pannacottaaaaa <mrjccaranay@mymail.mapua.edu.ph>
2024-07-13 17:00:02 +0800

    Update DegenToken.sol

--- ETH + AVAX Intermediate/Degen Token (ERC-20): Unlocking the Future of Gaming/DegenToken.sol
+++ ETH + AVAX Intermediate/Degen Token (ERC-20): Unlocking the Future of Gaming/DegenToken.sol
@@ -9,13 +9,19 @@ contract DegenToken is ERC20, Ownable {
     uint256 public constant REDEMPTION_RATE = 100;
 
     mapping(address => uint256) public itemsRedeemed;
+    mapping(string => uint256) public itemCosts;
 
     constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
         _mint(msg.sender, 10 * (10 ** uint256(decimals())));
+        // Example items and their costs
+        itemCosts["Sword"] = 100;
+        itemCosts["Shield"] = 150;
+        itemCosts["Potion"] = 50;
     }
 
-    function redeemItems(uint256 quantity) public {
-        uint256 cost = REDEMPTION_RATE * quantity;
+    function redeemItems(string memory itemName, uint256 quantity) public {
+        require(itemCosts[itemName] > 0, "Item does not exist");
+        uint256 cost = itemCosts[itemName] * quantity;
         require(balanceOf(msg.sender) >= cost, "Not enough tokens to redeem items");
 
         itemsRedeemed[msg.sender] += quantity;
@@ -37,5 +43,9 @@ contract DegenToken is ERC20, Ownable {
     function burnTokens(uint256 amount) public {
         require(balanceOf(msg.sender) >= amount, "Not enough tokens to burn");
         _burn(msg.sender, amount);
+    }
+
+    function setItemCost(string memory itemName, uint256 cost) public onlyOwner {
+        itemCosts[itemName] = cost;
     }
 }
